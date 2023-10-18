bd4bba25e6967484fdc692fa9bbedb8d829bbff8
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-10-18 11:51:47 +0430

    first commit

---
+++ src/main/java/com/mcit/SpringBootwithReactPackaging/SpringBootWithReactPackagingApplication.java
@@ -0,0 +1,13 @@
+package com.mcit.SpringBootwithReactPackaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithReactPackagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithReactPackagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcit/SpringBootwithReactPackaging/SpringBootWithReactPackagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcit.SpringBootwithReactPackaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactPackagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
